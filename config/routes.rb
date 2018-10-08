b270312fdec112a7439a3ec11808d97153282e07
lrlineroa <leonardoraulinero@gmail.com>
2018-10-08 13:01:43 -0500

    api versionada

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :positions
+
+	namespace :api do
+		namespace :v1 do
+	  		resources :users
+	  		resources :positions
+  		end
+  	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
